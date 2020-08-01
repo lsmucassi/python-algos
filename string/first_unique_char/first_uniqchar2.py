d693b7efbf206317837d25ddcc2760109a0cd79e
lsmucassi <lindasmucassi@gmail.com>
2020-08-01 21:37:07 +0200

    first uniq char opt 2

--- string/first_unique_char/first_uniqchar2.py
+++ string/first_unique_char/first_uniqchar2.py
@@ -7,12 +7,12 @@ import collections
 
 def first_uniqchar2(c):
     # build hash map of characters and count howmanytimes it appears
-    count = collections.Counters(c)
+    count = collections.Counter(c)
     # gives back a dictionary with words occurrence count
     # Counter({'l': 1, 'e': 3, 't': 1, 'c': 1, 'o': 1, 'd': 1})
 
     #find the index
-    for iidx, ch in enumerate(len(c)):
+    for idx, ch in enumerate(c):
         if count[ch] == 1:
             return idx
     return -1
