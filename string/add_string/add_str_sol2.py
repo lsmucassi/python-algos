994d1fc8064463acb304aaf7b3438069fbf0719c
lsmucassi <lindasmucassi@gmail.com>
2020-08-01 20:57:04 +0200

    add str two

--- string/add_string/add_str_sol2.py
+++ string/add_string/add_str_sol2.py
@@ -9,8 +9,18 @@ str2 = '1836'
 #first posible solution 
 
 def add_str2(str1, str2):
-    eval(str1) + eval(str2)
+    
+    n1, n2 = 0, 0
+    m1, m2 = 10**(len(str1)-1), 10**(len(str2)-1)
+
+    for i in str1:
+        n1 += (ord(i) - ord("0")) * m1
+        m1 = m1//10
+
+    for i in str2:
+        n2 += (ord(i) - ord("0")) * m2
+
     return str(eval(str1) + eval(str2))
 
-print(add_str1(str1, str2))
+print(add_str2(str1, str2))
 
