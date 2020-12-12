445db046985d8cf46d36f064e3fdb06c2a23aa56
lsmucassi <lindasmucassi@gmail.com>
2020-12-12 16:38:08 +0200

    fib tim complexity fix

--- prac/fib.py
+++ prac/fib.py
@@ -9,13 +9,16 @@ n:      1, 2, 3, 4, 5, 6, 7, 8, 9 ....
 fib:    1, 1, 2, 3, 5, 8, 13, 21, 34
 '''
 
-def fib(n):
+def fib(n, memo={}):
+    if n in memo:
+        return memo[n]
     if n <= 2:
         return 1
-    
-    return (fib(n -1) + fib(n -2))
+    memo[n] = fib(n -1, memo) + fib(n -2, memo)
+    return memo[n]
 
 
 print(fib(6))
 print(fib(7))
 print(fib(8))
+print(fib(50))
